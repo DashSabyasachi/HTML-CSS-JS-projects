76471a8b0543d552c4f0f603e7035a500295a891
Sabyasachi Dash <sdash0113@gmail.com>
2023-10-18 07:26:03 +0530

    config done

--- Box model/script.js
+++ Box model/script.js
@@ -4,20 +4,15 @@ let openBtn = document.getElementById('open-btn');
 let modalContainer = document.getElementById('modal-container');
 let closeBtn = document.getElementById('close-btn');
 
-// Event Listeners 
-
-openBtn.addEventListener('click', function(){
-
+openBtn.addEventListener('click', ()=>{
     modalContainer.style.display = 'block';
 });
 
-closeBtn.addEventListener('click', function() {
-
+closeBtn.addEventListener('click', ()=> {
     modalContainer.style.display = 'none';
 });
 
-window.addEventListener('click', function(e) {
-
+window.addEventListener('click', (e)=> {
     if (e.target === modalContainer) {
         modalContainer.style.display = 'none';
     }
