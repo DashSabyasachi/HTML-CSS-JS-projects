cd004d9718e140edf35295485e163417ca590319
Sabyasachi Dash <sdash0113@gmail.com>
2023-10-24 17:42:44 +0530

    config done

--- Cart Management/script.js
+++ Cart Management/script.js
@@ -1,15 +1,15 @@
 const product = [
     {
         id: 0,
-        image: 'image/gg-1.jpg',
-        title: 'Z Flip Foldable Mobile',
-        price: 120000,
+        image: 'image/bb-0.png',
+        title: 'Book- We Could Be So Good',
+        price: 500,
     },
     {
         id: 1,
-        image: 'image/hh-2.jpg',
-        title: 'Air Pods Pro',
-        price: 6000,
+        image: 'image/rr-1.png',
+        title: 'Gold Ring',
+        price: 17000,
     },
     {
         id: 2,
@@ -19,11 +19,24 @@ const product = [
     },
     {
         id: 3,
+        image: 'image/hh-2.jpg',
+        title: 'Ear Buds',
+        price: 5000,
+    },
+    {
+        id: 4,
+        image: 'image/gg-1.jpg',
+        title: '250D DSLR Camera',
+        price: 23000,
+    },
+    {
+        id: 5,
         image: 'image/aa-1.jpg',
         title: 'Head Phones',
         price: 1000,
     }
 ];
+
 const categories = [...new Set(product.map((item)=>
     {return item}))]
     let i=0;
@@ -54,20 +67,38 @@ function delElement(a){
     cart.splice(a, 1);
     displaycart();
 }
+function clearCart() {
+    cart = [];
+    displaycart(); 
+}
 
 function displaycart(){
-    let j = 0, total=0;
+    let j = 0, total=0 ,sf = 0 ;
+
+    for (const item of cart) {
+        total += item.price;
+    }
+
+    if (total > 10000) {
+        sf = 0;
+    } else {
+        sf = 500;
+    }
+
+    const gt = total + sf; 
     document.getElementById("count").innerHTML=cart.length;
     if(cart.length==0){
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
         document.getElementById("total").innerHTML = "₹ "+0+".00";
+        document.getElementById("sf").innerHTML = "₹ "+0+".00";
+        document.getElementById("gt").innerHTML = "₹ "+0+".00";
+    
     }
     else{
         document.getElementById("cartItem").innerHTML = cart.map((items)=>
         {
             var {image, title, price} = items;
-            total=total+price;
-            document.getElementById("total").innerHTML = "₹ "+total+".00";
+          
             return(
                 `<div class='cart-item'>
                 <div class='row-img'>
@@ -78,7 +109,9 @@ function displaycart(){
                 "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
             );
         }).join('');
+        document.getElementById("total").innerHTML = "₹ "+total+".00";
+        document.getElementById("sf").innerHTML = "₹ "+sf+".00";
+        document.getElementById("gt").innerHTML = "₹ "+gt+".00";
     }
-
-    
+ 
 }
