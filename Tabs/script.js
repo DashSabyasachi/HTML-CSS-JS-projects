6b97e04837dd0ac628336366c8f3a717ca95d038
Sabyasachi Dash <sdash0113@gmail.com>
2023-10-25 10:55:16 +0530

    config done

---
+++ Tabs/script.js
@@ -0,0 +1,15 @@
+let tabs = document.querySelectorAll(".tabs h3");
+let tabContents = document.querySelectorAll(".tab-content div");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content) => {``
+      content.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabContents[index].classList.add("active");
+    tabs[index].classList.add("active");
+  });
+});
